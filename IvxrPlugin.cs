f894f85ea671f4554ebcb23d86c3c3c8e6a32c9a
Premek Paska <premysl.paska@goodai.com>
2020-04-15 17:50:16 +0200

    Added a session component and checked it's active. Formatting.

--- IvxrPlugin.cs
+++ IvxrPlugin.cs
@@ -5,54 +5,61 @@ using VRage.Utils;
 
 namespace EU.Iv4xr.SePlugin
 {
-    public class IvxrPlugin : IPlugin
-    {
-        MyLog m_log;
+	public class IvxrPlugin : IPlugin
+	{
+		public static MyLog Log { get; private set; } 
 
-        public void Init(object gameInstance)
-        {
-            m_log = new MyLog(alwaysFlush: true);
-            m_log.Init("ivxr-plugin.log", new System.Text.StringBuilder("0.1.0"));
-            m_log.WriteLine($"{nameof(IvxrPlugin)} initialization started.");
-        }
+		static IvxrPlugin()
+		{
+			Log = new MyLog(alwaysFlush: true);
+			Log.Init("ivxr-plugin.log", new System.Text.StringBuilder("0.1.0"));
+		}
 
-        public void Update()
-        {
-        }
+		public void Init(object gameInstance)
+		{
+			Log.WriteLine($"{nameof(IvxrPlugin)} initialization started.");
+		}
 
-        #region IDisposable Support
-        private bool disposedValue = false; // To detect redundant calls
+		public void Update()
+		{
+			//m_log.WriteLine("Update called.");
+		}
 
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    // TODO: dispose managed state (managed objects).
-                }
 
-                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-                // TODO: set large fields to null.
 
-                disposedValue = true;
-            }
-        }
+		#region IDisposable Support
+		private bool disposedValue = false; // To detect redundant calls
 
-        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-        // ~PluginMain() {
-        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-        //   Dispose(false);
-        // }
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposedValue)
+			{
+				if (disposing)
+				{
+					// TODO: dispose managed state (managed objects).
+				}
 
-        // This code added to correctly implement the disposable pattern.
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-            Dispose(true);
-            // TODO: uncomment the following line if the finalizer is overridden above.
-            // GC.SuppressFinalize(this);
-        }
-        #endregion
-    }
+				// TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+				// TODO: set large fields to null.
+
+				disposedValue = true;
+			}
+		}
+
+		// TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+		// ~PluginMain() {
+		//   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+		//   Dispose(false);
+		// }
+
+		// This code added to correctly implement the disposable pattern.
+		public void Dispose()
+		{
+			// Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+			Dispose(true);
+			// TODO: uncomment the following line if the finalizer is overridden above.
+			// GC.SuppressFinalize(this);
+		}
+		#endregion
+	}
 }
---
+++ IvxrSession.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VRage.Game.Components;
+
+namespace EU.Iv4xr.SePlugin
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.BeforeSimulation | MyUpdateOrder.AfterSimulation)]
+    class IvxrSession : MySessionComponentBase
+    {
+        public override void UpdateBeforeSimulation()
+        {
+            base.UpdateBeforeSimulation();
+            IvxrPlugin.Log.WriteLine("IvxrSession: *** Update before simulation ***");
+        }
+    }
+}
