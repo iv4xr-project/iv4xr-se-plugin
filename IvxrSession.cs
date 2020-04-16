9eb3373defd92272cb0c917bde9d55790f16f7ab
Premek Paska <premysl.paska@goodai.com>
2020-04-16 18:23:37 +0200

    Spike solution of getting player's position and retrieving some entities.

--- IvxrSession.cs
+++ IvxrSession.cs
@@ -1,7 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Havok;
+using Sandbox.Game.Entities;
 using VRage.Game.Components;
+using VRageMath;
+using Sandbox.Game.Multiplayer;
+using VRage.Game.Entity;
 
 namespace EU.Iv4xr.SePlugin
 {
@@ -11,7 +17,22 @@ namespace EU.Iv4xr.SePlugin
         public override void UpdateBeforeSimulation()
         {
             base.UpdateBeforeSimulation();
-            IvxrPlugin.Log.WriteLine("IvxrSession: *** Update before simulation ***");
+
+            var playerCount = Sync.Players.GetOnlinePlayerCount();
+            if (playerCount > 0)
+            {
+                var player = Sync.Players.GetOnlinePlayers().First();
+
+                var characterPosition = player.Character.PositionComp.GetPosition();
+                
+                var sphere = new BoundingSphereD(characterPosition, radius: 25.0);
+                List<MyEntity> entities = MyEntities.GetEntitiesInSphere(ref sphere);
+
+                IvxrPlugin.Log.WriteLine(
+                    $"IvxrSession: position: {characterPosition.X}, entities count: {entities.Count}");
+                
+                entities.Clear();
+            }
         }
     }
 }
