2ed9ed1f15e237d7399b33e951b88a1830e3a23a
Premek Paska <premysl.paska@goodai.com>
2020-05-02 14:58:37 +0200

    Se server mock initial commit.
    . Mock server minor update.

---
+++ Helpers/GameServerMock/SeServerMock/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SeServerMock
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var server = new Server();
+            server.Start();
+        }
+    }
+}
