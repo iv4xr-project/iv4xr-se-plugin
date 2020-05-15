74712f784adf293807ac1da2a18b2f3db52cd750
Premek Paska <premysl.paska@goodai.com>
2020-05-15 12:40:30 +0200

    Simplify the server.

--- Source/Ivxr.PlugIndependentLib/Comm/PluginServer.cs
+++ Source/Ivxr.PlugIndependentLib/Comm/PluginServer.cs
@@ -94,30 +94,22 @@ namespace EU.Iv4xr.PluginLib
         {
             var buffer = new byte[4096];
 
-            while (!m_shouldStop)
+            int readCount;
+            while ((readCount = stream.Read(buffer, 0, buffer.Length)) != 0)
             {
-                int readCount;
-                int readSoFar = 0;
-                while ((readCount = stream.Read(buffer, readSoFar, buffer.Length - readSoFar)) != 0)
-                {
-                    readSoFar += readCount;
-                    if (readSoFar >= buffer.Length * 3 / 4)
-                        throw new InternalBufferOverflowException("Buffer too small (TODO: grow it).");
+                string message = Encoding.ASCII.GetString(buffer, 0, readCount);
+                int indexOfNewLine = message.IndexOf('\n');
+                if ((indexOfNewLine != -1) && (indexOfNewLine != message.Length - 1))
+                    throw new NotImplementedException("Unexpected new line in the middle of message.");
 
-                    string message = Encoding.ASCII.GetString(buffer, 0, readSoFar);
-                    int indexOfNewLine = message.IndexOf('\n');
-                    if ((indexOfNewLine != -1) && (indexOfNewLine != message.Length - 1))
-                        throw new NotImplementedException("Unexpected new line in the middle of message.");
+                if (indexOfNewLine == -1)
+                    throw new NotImplementedException("Reading message in multiple parts not implemented.");
 
-                    m_log.WriteLine($"Read message: {message}");
+                m_log.WriteLine($"Read message: {message}");
 
-                    // FIXME: just a testing reply
-                    var replyBuffer = Encoding.ASCII.GetBytes($"Got {readCount} bytes, thanks.\n");
-                    stream.Write(replyBuffer, 0, replyBuffer.Length);
-
-                    // we are only doing this because we don't support reading multiple parts...
-                    readSoFar = 0;
-                }
+                // FIXME: just a testing reply
+                var replyBuffer = Encoding.ASCII.GetBytes($"Got {readCount} bytes, thanks.\n");
+                stream.Write(replyBuffer, 0, replyBuffer.Length);
             }
         }
     }
