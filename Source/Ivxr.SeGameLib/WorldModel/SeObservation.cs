d8d5ba21327535f7d9ee72ee9c7404fcc9c7bb5e
Premek Paska <premysl.paska@goodai.com>
2020-05-29 15:28:42 +0200

    Add a new project dependent on SE code.

---
+++ Source/Ivxr.SeGameLib/WorldModel/SeObservation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VRageMath;
+
+namespace EU.Iv4xr.SeGameLib.WorldModel
+{
+	// Intended to be maximally compatible with eu.iv4xr.framework.world.WorldModel from the iv4XR Java framework.
+	public class SeObservation
+	{
+		public string AgentId;
+
+		public Vector3D Position;  // Agent's position
+		public Vector3D Velocity;  // Agent's velocity
+		public Vector3D Extent;  // Agent's dimensions (x,y,z size/2)
+
+		// TODO(PP): Add entities.
+	}
+}
