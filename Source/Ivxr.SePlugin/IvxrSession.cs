7bd4ceb737f57296d9765ff4b9ce023c493d39b9
Premysl Paska <premysl.paska@goodai.com>
2020-06-18 16:39:13 +0200

    Remove some unused code.

--- Source/Ivxr.SePlugin/IvxrSession.cs
+++ Source/Ivxr.SePlugin/IvxrSession.cs
@@ -1,19 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Havok;
-using Sandbox.Game.Entities;
-using VRage.Game.Components;
-using VRageMath;
-using Sandbox.Game.Multiplayer;
-using VRage.Game.Entity;
-using LitJson;
-using EU.Iv4xr.SePlugin.WorldModel;
+﻿using VRage.Game.Components;
 
 namespace EU.Iv4xr.SePlugin
 {
-    [MySessionComponentDescriptor(MyUpdateOrder.BeforeSimulation | MyUpdateOrder.AfterSimulation)]
+	[MySessionComponentDescriptor(MyUpdateOrder.BeforeSimulation | MyUpdateOrder.AfterSimulation)]
     class IvxrSession : MySessionComponentBase
     {
         public override void UpdateBeforeSimulation()
