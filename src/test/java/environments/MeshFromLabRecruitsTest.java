c733e88d0950743c621dde10417a9ed35c48a5b5
ISWB Prasetya <>
2020-08-09 23:40:32 +0200

    adding a test for the navmesh sent by LR

--- src/test/java/environments/MeshFromLabRecruitsTest.java
+++ src/test/java/environments/MeshFromLabRecruitsTest.java
@@ -1,13 +1,14 @@
 package environments;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.* ;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import eu.iv4xr.framework.spatial.Vec3;
 import eu.iv4xr.framework.spatial.meshes.Face;
+import eu.iv4xr.framework.spatial.meshes.Mesh;
 import game.LabRecruitsTestServer;
 import game.Platform;
 import world.LabEntity;
@@ -38,21 +39,102 @@ public class MeshFromLabRecruitsTest {
     	if(labRecruitsTestServer!=null) labRecruitsTestServer.close(); 
     }
     
+    /**
+     * Some basic checks on the nav-mesh sent by Lab Recruits. 
+     */
     @Test
     public void test_mesh() {
     	assertTrue(labRecruitsTestServer != null) ;
-    	var env = new LabRecruitsEnvironment(new LabRecruitsConfig("minimal"));
-    	System.out.println(env.worldNavigableMesh) ;
-    	var connectedComponents = env.worldNavigableMesh.getConnectedComponets() ;
-    	System.out.println("#Connected components = " + connectedComponents.size()) ;
-    	int k = 0 ;
-    	for (var group : connectedComponents) {
-    		System.out.println("** Component " + k) ;
-    		for (Face f : group) {
-    			System.out.println("   Face " + f.toString(env.worldNavigableMesh.vertices)) ;
-    		}
-    		k++ ;
-    	}
+        // test using the "minimal" level:
+    	var level = "minimal" ;
+    	System.out.println(">>> level: " + level) ;
+    	var env = new LabRecruitsEnvironment(new LabRecruitsConfig(level));
+    	var obs = env.observe("agent0") ;
+    	System.out.println(">>> agent position:" + obs.position) ;
+    	var mesh = env.worldNavigableMesh ;
+    	System.out.println(">>> #vertices: " + mesh.vertices.size()) ;
+    	System.out.println(">>> #faces: " + mesh.faces.size()) ;
+    	var bottomleft = getBottomLeftCorner(mesh) ;
+    	var topright = getTopRightCorner(mesh); 
+    	System.out.println(">>> bottom-left: " + bottomleft) ;
+    	System.out.println(">>> top-right: " + topright) ;
+    	assertTrue(Vec3.dist(bottomleft, new Vec3(-0.2f,1f,-0.2f)) <= 0.1) ;
+    	assertTrue(Vec3.dist(topright, new Vec3(2.2f,1f,3.2f)) <= 0.1) ;
+    	
+    	// using a horizontal corridor-shaped level:
+    	level = "longcorridor" ;
+    	System.out.println(">>> level: " + level) ;
+    	env = new LabRecruitsEnvironment(new LabRecruitsConfig(level));
+    	obs = env.observe("agent0") ;
+    	System.out.println(">>> agent position:" + obs.position) ;
+    	var meshHorzCorridor = env.worldNavigableMesh ;
+    	mesh = meshHorzCorridor ;
+    	System.out.println(">>> #vertices: " + mesh.vertices.size()) ;
+    	System.out.println(">>> #faces: " + mesh.faces.size()) ;
+    	bottomleft = getBottomLeftCorner(mesh) ;
+    	topright = getTopRightCorner(mesh); 
+    	System.out.println(">>> bottom-left: " + bottomleft) ;
+    	System.out.println(">>> top-right: " + topright) ;
+    	assertTrue(Vec3.dist(bottomleft, new Vec3(-0.2f,1f,-0.2f)) <= 0.1) ;
+    	assertTrue(Vec3.dist(topright, new Vec3(10.2f,1f,2.2f)) <= 0.1) ;
+    	
+    	// using a vertical corridor-shaped level:
+    	level = "longcorridor2" ;
+    	System.out.println(">>> level: " + level) ;
+    	env = new LabRecruitsEnvironment(new LabRecruitsConfig(level));
+    	obs = env.observe("agent0") ;
+    	System.out.println(">>> agent position:" + obs.position) ;
+    	mesh = env.worldNavigableMesh ;
+    	System.out.println(">>> #vertices: " + mesh.vertices.size()) ;
+    	System.out.println(">>> #faces: " + mesh.faces.size()) ;
+    	bottomleft = getBottomLeftCorner(mesh) ;
+    	topright = getTopRightCorner(mesh); 
+    	System.out.println(">>> bottom-left: " + bottomleft) ;
+    	System.out.println(">>> top-right: " + topright) ;
+    	assertTrue(Vec3.dist(bottomleft, new Vec3(-0.2f,1f,-0.2f)) <= 0.1) ;
+    	assertTrue(Vec3.dist(topright, new Vec3(2.2f,1f,10.2f)) <= 0.1) ;
+    	
+    	// adding a door does not change the numer of vertices nor faces. Compare this
+    	// with the long-corridor level:
+    	level = "longcorridorWithDoorBetween" ;
+    	System.out.println(">>> level: " + level) ;
+    	env = new LabRecruitsEnvironment(new LabRecruitsConfig(level));
+    	mesh = env.worldNavigableMesh ;
+    	assertEquals(mesh.vertices.size(), meshHorzCorridor.vertices.size()) ;
+    	assertEquals(mesh.faces.size(), meshHorzCorridor.faces.size()) ;
+    	
     }
+    
+    Vec3 getBottomLeftCorner(Mesh mesh) {
+    	float minX = Float.MAX_VALUE ;
+    	float minZ = Float.MAX_VALUE ;
+    	for (Vec3 p : mesh.vertices) {
+    		minX = Math.min(minX, p.x) ;
+    		minZ = Math.min(minZ, p.z) ;
+    	}
+    	for (Vec3 p : mesh.vertices) {
+    		if (p.x == minX && p.z == minZ) {
+    			return p ;
+    		}
+    	}
+    	return null ;
+    }
+    
+    Vec3 getTopRightCorner(Mesh mesh) {
+    	float maxX = Float.MIN_VALUE ;
+    	float maxZ = Float.MIN_VALUE ;
+    	for (Vec3 p : mesh.vertices) {
+    		maxX = Math.max(maxX, p.x) ;
+    		maxZ = Math.max(maxZ, p.z) ;
+    	}
+    	for (Vec3 p : mesh.vertices) {
+    		if (p.x == maxX && p.z == maxZ) {
+    			return p ;
+    		}
+    	}
+    	return null ;
+    }
+    
+    
 
 }
