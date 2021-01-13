dd4e4df464c489346a4b0f8f90b0f571ac132d35
Premysl Paska <premysl.paska@goodai.com>
2021-01-13 16:42:06 +0100

    Add a test checking for a new block.

--- src/test/java/spaceEngineers/InteractionTest.java
+++ src/test/java/spaceEngineers/InteractionTest.java
@@ -1,6 +1,5 @@
 package spaceEngineers;
 
-import eu.iv4xr.framework.spatial.Vec3;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import spaceEngineers.commands.InteractionArgs;
@@ -32,6 +31,33 @@ public class InteractionTest {
         observation = environment.getSeResponse(SeRequest.command(
                 SeAgentCommand.interact("you", new InteractionArgs(InteractionType.PLACE))));
         Assertions.assertNotNull(observation);
+
+        boolean result = environment.getSeResponse(SeRequest.disconnect());
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void checkNewBlock() {
+        var environment = SpaceEngEnvironment.localhost();
+
+        var observation = environment.getSeResponse(SeRequest.command(
+                SeAgentCommand.interact("you", new InteractionArgs(InteractionType.EQUIP,3))));
+        Assertions.assertNotNull(observation);
+
+        observation = environment.getSeResponse(SeRequest.command(
+                SeAgentCommand.interact("you", new InteractionArgs(InteractionType.PLACE))));
+        Assertions.assertNotNull(observation);
+
+        observation = environment.getSeResponse(SeRequest.command(SeAgentCommand.observe("you")));
+        Assertions.assertNotNull(observation);
+        Assertions.assertNotNull(observation.blocks);
+        Assertions.assertTrue(observation.blocks.size() > 0);
+
+        System.out.println("Got " + observation.blocks.size() + " blocks.");
+        var firstBlock = observation.blocks.get(0);
+        System.out.println("First block max integrity: " + firstBlock.maxIntegrity);
+        System.out.println("First block build integrity: " + firstBlock.buildIntegrity);
+        System.out.println("First block integrity: " + firstBlock.integrity);
 
         boolean result = environment.getSeResponse(SeRequest.disconnect());
         Assertions.assertTrue(result);
