940e29b6d9e42c49a84c0f12ed42056fef3f21f4
Karel Hovorka <git@karel-hovorka.eu>
2021-02-18 01:06:55 +0100

    Suggested idea to refactor Dispatcher commands to be more flexible when adding new commands.

--- Source/Ivxr.SePlugin/Control/Dispatcher.cs
+++ Source/Ivxr.SePlugin/Control/Dispatcher.cs
@@ -1,8 +1,6 @@
 ﻿using Iv4xr.PluginLib;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Text;
 using Iv4xr.SePlugin.Json;
 using Iv4xr.SePlugin.WorldModel;
 using Iv4xr.PluginLib.Comm;
@@ -10,86 +8,186 @@ using VRageMath;
 
 namespace Iv4xr.SePlugin.Control
 {
-	public class Dispatcher
-	{
-		public ILog Log { get; set; }
 
-		private readonly RequestQueue m_requestQueue;
+    using CommandDict = Dictionary<String, StringCommand>;
 
-		private readonly IObserver m_observer;
+    public class DispatcherContext
+    {
+        public readonly IObserver observer;
+        public readonly ICharacterController characterController;
 
-		private readonly ICharacterController m_controller;
+        public DispatcherContext(IObserver observer, ICharacterController characterController)
+        {
+            this.observer = observer;
+            this.characterController = characterController;
+        }
+    }
 
-		private readonly Jsoner m_jsoner = new Jsoner();
+    public interface StringCommand
+    {
+        string Cmd { get; }
+        string Execute(string message, DispatcherContext m_context, Jsoner m_jsoner);
+    }
 
-		public Dispatcher(RequestQueue requestQueue, IObserver observer, ICharacterController controller)
-		{
-			m_requestQueue = requestQueue;
-			m_observer = observer;
-			m_controller = controller;
-		}
+    public abstract class DispatcherCommand<I, O> : StringCommand
+        where I : class
+        where O : class
+    {
+        public string Cmd { get; }
 
-		public void ProcessRequests()
-		{
-			while (m_requestQueue.Requests.TryDequeue(out RequestItem request))
-			{
-				string jsonReply;
+        public DispatcherCommand(string cmd)
+        {
+            this.Cmd = cmd;
+        }
 
-				try
-				{
-					jsonReply = ProcessSingleRequest(request);
-				}
-				catch (Exception ex)
-				{
-					Log.Exception(ex, "Error processing a request");
-					Log.WriteLine($"Full request: \"{request.Message}\"");
-					jsonReply = "false";  // Simple error response, details can be learned from the log.
-				}
+        public string Execute(string message, DispatcherContext m_context, Jsoner m_jsoner)
 
-				m_requestQueue.Replies.Add(
-					new RequestItem(request.ClientStream, message: jsonReply));
-			}
-		}
+        {
+            var inputData = m_jsoner.ToObject<I>(message);
+            var outputData = Execute(m_context, inputData);
+            return m_jsoner.ToJson(outputData);
+        }
 
-		private string ProcessSingleRequest(RequestItem request)
-		{
-			// Skip prefix "{\"Cmd\":\"AGENTCOMMAND\",\"Arg\":{\"Cmd\":\""
-			var commandName = request.Message.Substring(36, 10);
+        public abstract O Execute(DispatcherContext context, I data);
+    }
 
-			Log?.WriteLine($"{nameof(Dispatcher)} command prefix: '{commandName}'.");
+    public class ObserveCommand : DispatcherCommand<SeRequestShell<AgentCommand<ObservationArgs>>, SeObservation>
+    {
 
-			if (commandName.StartsWith("OBSERVE") || commandName.StartsWith("DONOTHING"))  // DONOTHING is obsolete.
-			{
-				var requestShell = m_jsoner.ToObject<SeRequestShell<AgentCommand<ObservationArgs>>>(request.Message);
+        public ObserveCommand() : base("OBSERVE")
+        {}
 
-				return m_jsoner.ToJson(m_observer.GetObservation(requestShell.Arg.Arg));
-			}
-			else if (commandName.StartsWith("MOVE_ROTAT"))  // MOVE_ROTATE
-			{
-				var requestShell = m_jsoner.ToObject<SeRequestShell<AgentCommand<MoveAndRotateArgs>>>(request.Message);
+        public override SeObservation Execute(DispatcherContext context, SeRequestShell<AgentCommand<ObservationArgs>> data)
+        {
+            return context.observer.GetObservation(data.Arg.Arg);
+        }
+    }
 
-				m_controller.Move(requestShell.Arg.Arg);
-			}
-			else if (commandName.StartsWith("MOVETOWARD"))  // TODO(PP): Remove.
-			{
-				var requestShell = m_jsoner.ToObject<SeRequestShell<AgentCommand<MoveCommandArgs>>>(request.Message);
-				var moveCommandArgs = requestShell.Arg.Arg;
+    public class MoveAndRotateCommand : DispatcherCommand<SeRequestShell<AgentCommand<MoveAndRotateArgs>>, SeObservation>
+    {
+        public MoveAndRotateCommand() : base("MOVE_ROTATE")
+        {}
 
-				Log?.WriteLine($"Move indicator: {moveCommandArgs.MoveIndicator}");
-				m_controller.Move(moveCommandArgs.MoveIndicator, Vector2.Zero, 0.0f);
-			}
-			else if (commandName.StartsWith("INTERACT"))
-			{ 
-				var requestShell = m_jsoner.ToObject<SeRequestShell<AgentCommand<InteractionArgs>>>(request.Message);
+        public override SeObservation Execute(DispatcherContext context, SeRequestShell<AgentCommand<MoveAndRotateArgs>> data)
+        {
+            context.characterController.Move(data.Arg.Arg);
+            return context.observer.GetObservation();
+        }
+    }
 
-				m_controller.Interact(requestShell.Arg.Arg);
-			}
-			else
-			{
-				throw new NotImplementedException($"Unknown agent command: {commandName}");
-			}
+    public class MoveTowardCommand : DispatcherCommand<SeRequestShell<AgentCommand<MoveCommandArgs>>, SeObservation>
+    {
+        public MoveTowardCommand() : base("MOVETOWARD")
+        {}
 
-			return m_jsoner.ToJson(m_observer.GetObservation());
-		}
-	}
+        public override SeObservation Execute(DispatcherContext context, SeRequestShell<AgentCommand<MoveCommandArgs>> data)
+        {
+            context.characterController.Move(data.Arg.Arg.MoveIndicator, Vector2.Zero, 0.0f);
+            return context.observer.GetObservation();
+
+        }
+    }
+
+    public class InteractCommand : DispatcherCommand<SeRequestShell<AgentCommand<InteractionArgs>>, SeObservation>
+    {
+        public InteractCommand() : base("INTERACT")
+        {}
+
+        public override SeObservation Execute(DispatcherContext context, SeRequestShell<AgentCommand<InteractionArgs>> data)
+        {
+            context.characterController.Interact(data.Arg.Arg);
+            return context.observer.GetObservation();
+        }
+    }
+
+
+    public class Dispatcher
+    {
+        public ILog Log { get; set; }
+
+        private readonly RequestQueue m_requestQueue;
+
+        private readonly IObserver m_observer;
+
+        private readonly ICharacterController m_controller;
+
+        private readonly Jsoner m_jsoner = new Jsoner();
+
+        private readonly DispatcherContext m_context;
+
+        private readonly CommandDict m_commands;
+
+        public Dispatcher(RequestQueue requestQueue, IObserver observer, ICharacterController controller, CommandDict commands = null)
+        {
+            m_requestQueue = requestQueue;
+            m_observer = observer;
+            m_controller = controller;
+            m_context = new DispatcherContext(observer, controller);
+            m_commands = commands;
+            if (m_commands == null)
+            {
+                m_commands = DefaultCommands();
+            }
+        }
+
+        public void AddCommand(StringCommand command)
+        {
+            m_commands[command.Cmd] = command;
+        }
+
+        private CommandDict DefaultCommands()
+        {
+
+            var commandList = new List<StringCommand>
+            {
+                new ObserveCommand(),
+                new MoveAndRotateCommand(),
+                new MoveTowardCommand(),
+                new InteractCommand()
+            };
+            var result = new CommandDict();
+            foreach (var command in commandList)
+            {
+                result[command.Cmd] = command;
+            }
+            return result;
+        }
+
+        public void ProcessRequests()
+        {
+            while (m_requestQueue.Requests.TryDequeue(out RequestItem request))
+            {
+                string jsonReply;
+
+                try
+                {
+                    jsonReply = ProcessSingleRequest(request);
+                }
+                catch (Exception ex)
+                {
+                    Log.Exception(ex, "Error processing a request");
+                    Log.WriteLine($"Full request: \"{request.Message}\"");
+                    jsonReply = "false";  // Simple error response, details can be learned from the log.
+                }
+
+                m_requestQueue.Replies.Add(
+                    new RequestItem(request.ClientStream, message: jsonReply));
+            }
+        }
+
+        private string ProcessSingleRequest(RequestItem request)
+        {
+            // Skip prefix "{\"Cmd\":\"AGENTCOMMAND\",\"Arg\":{\"Cmd\":\""
+            var commandName = request.Message.Substring(36, 20).Split(new string[] { "\"" }, StringSplitOptions.None)[0];
+            Log?.WriteLine($"{nameof(Dispatcher)} command prefix: '{commandName}'.");
+
+            if (m_commands.ContainsKey(commandName))
+            {
+                var command = m_commands[commandName];
+                return command.Execute(request.Message, m_context, m_jsoner);
+            }
+            throw new NotImplementedException($"Unknown agent command: {commandName}");
+
+        }
+    }
 }
