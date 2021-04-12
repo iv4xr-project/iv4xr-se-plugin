f8974ab1b32d2eeb3c003439a236cee0df2b22eb
Karel Hovorka <git@karel-hovorka.eu>
2021-04-12 15:41:34 +0200

    Updated rotate test.

--- src/jvmTest/kotlin/spaceEngineers/game/RotateTowardsPositionTest.kt
+++ src/jvmTest/kotlin/spaceEngineers/game/RotateTowardsPositionTest.kt
@@ -1,12 +1,12 @@
 package spaceEngineers.game
 
 import spaceEngineers.controller.blockingRotateUntilOrientationForward
-import spaceEngineers.controller.blockingRotateUntilOrientationUp
 import spaceEngineers.controller.loadFromTestResources
 import spaceEngineers.model.ToolbarLocation
 import spaceEngineers.model.Vec3
 import spaceEngineers.model.allBlocks
-import testhelp.*
+import testhelp.assertVecEquals
+import testhelp.spaceEngineers
 import java.lang.Thread.sleep
 import kotlin.test.Test
 
@@ -20,27 +20,19 @@ class RotateTowardsPositionTest {
         session.loadFromTestResources(scenarioId)
         sleep(1000)
         observer.observeNewBlocks()
-        items.equip(ToolbarLocation(0, 0))
+        items.equip(ToolbarLocation(1, 0))
         sleep(1000)
         items.place()
         sleep(1000)
         items.equip(ToolbarLocation(9, 0))
 
 
-        val newBlocksObservation = observer.observeNewBlocks()
-        val block = newBlocksObservation.allBlocks.first()
+        val type = "LargeHeavyBlockArmorBlock"
+        val newBlocksObservation = observer.observeBlocks()
+        val block = newBlocksObservation.allBlocks.first { it.blockType == type }
         val orientationTowardsBlock = (block.position - newBlocksObservation.position).normalized()
         assertVecEquals(Vec3(-0.997f, 0.066f, 0.02f), orientationTowardsBlock, diff = 0.1f)
         assertVecEquals(orientationTowardsBlock, newBlocksObservation.orientationForward, diff = 0.1f)
-
-        character.blockingRotateUntilOrientationUp(
-            finalOrientation = Vec3(-1f, 0f, 0f),
-            rotation = Vec3.ROTATE_UP,
-            maxTries = 999999
-        )
-        val initialCharacterObservation = observer.observe()
-        assertVecEquals(Vec3(-1f, 0.0f, 0.0f), initialCharacterObservation.orientationForward)
-
 
         character.blockingRotateUntilOrientationForward(
             finalOrientation = Vec3(0f, 0f, -1f),
