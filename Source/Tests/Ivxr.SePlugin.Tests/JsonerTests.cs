ac182b28ea9ce448371fe13a713e7c3ca8a2478a
Karel Hovorka <git@karel-hovorka.eu>
2021-07-14 13:12:08 +0200

    Extracted some jsoner tests to common module.

--- Source/Tests/Ivxr.SePlugin.Tests/JsonerTests.cs
+++ Source/Tests/Ivxr.SePlugin.Tests/JsonerTests.cs
@@ -8,48 +8,12 @@ using Iv4xr.SePlugin.Control;
 
 namespace Ivxr.SeGameLib.Tests
 {
-    public class JsonerTests
+    public class JsonerSeTests
     {
         private readonly IJsoner m_jsoner = new Jsoner();
 
         private const int Precision = 4;
-
-        private class Class
-        {
-            public int Number = 9;
-            public string Message = "Dragon lands.";
-        }
-
-        [Fact]
-        public void ConvertsObjectToJson()
-        {
-            var obj = new Class();
-
-            Assert.Equal("{\"Number\":9,\"Message\":\"Dragon lands.\"}", m_jsoner.ToJson(obj));
-        }
-
-        [Fact]
-        public void ConvertsSeObservationToJson()
-        {
-            var observation = new CharacterObservation()
-            {
-                    Id = "Foo",
-                    Position = new PlainVec3D(1, 2, 3)
-            };
-
-            var json = m_jsoner.ToJson(observation);
-            // File.WriteAllText("observation.json", json);
-
-            Assert.Equal("{\"Velocity\":{\"X\":0.0,\"Y\":0.0,\"Z\":0.0}," +
-                         "\"Extent\":{\"X\":0.0,\"Y\":0.0,\"Z\":0.0}," +
-                         "\"Camera\":null,\"JetpackRunning\":false," +
-                         "\"HeadLocalXAngle\":0.0,\"HeadLocalYAngle\":0.0,\"TargetBlock\":null,\"Id\":\"Foo\"," +
-                         "\"Position\":{\"X\":1.0,\"Y\":2.0,\"Z\":3.0}," +
-                         "\"OrientationForward\":{\"X\":0.0,\"Y\":0.0,\"Z\":0.0}," +
-                         "\"OrientationUp\":{\"X\":0.0,\"Y\":0.0,\"Z\":0.0}}",
-                json);
-        }
-
+        
         [Fact]
         public void DeserializesPlainVec3DIntoVector3D()
         {
