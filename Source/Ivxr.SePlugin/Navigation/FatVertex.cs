d7d4f584e61cc3aa30292125a18cf6a94fd8b14a
Premysl Paska <premysl.paska@goodai.com>
2021-09-02 17:03:45 +0200

    Navigation WIP
    .

---
+++ Source/Ivxr.SePlugin/Navigation/FatVertex.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Iv4xr.PluginLib.WorldModel;
+
+namespace Iv4xr.SePlugin.Navigation
+{
+    /// <summary>
+    /// Apart from position, stores also edges leading to connected vertices.
+    /// </summary>
+    public class FatVertex
+    {
+        public readonly PlainVec3D Position;
+
+        public readonly List<FatVertex> Neighbours = new List<FatVertex>(capacity: 8);
+
+    }
+}
