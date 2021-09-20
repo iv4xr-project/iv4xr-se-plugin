3dacdac3d746568205bad3f5a6ac5787083987e2
Premysl Paska <premysl.paska@goodai.com>
2021-09-20 16:40:05 +0200

    . test WIP

---
+++ Source/Tests/Ivxr.SePlugin.Tests/NavGraphTests.cs
@@ -0,0 +1,24 @@
+﻿
+using Iv4xr.SePlugin.Control;
+using Iv4xr.SePlugin.Navigation;
+using Sandbox.Game.Entities;
+using Sandbox.Game.Entities.Character;
+using VRageMath;
+using Xunit;
+
+namespace Ivxr.SeGameLib.Tests
+{
+    public class NavGraphTests
+    {
+        [Fact]
+        public void CreatesGraph()
+        {
+            // We will not call methods that use LowLevelObserver
+            var graphEditor = new NavGraphEditor(new LowLevelObserver(new GameSession()));
+
+            var grid = new MyCubeGrid();
+            //grid.BuildGeneratedBlock()
+            var graph = graphEditor.CreateGraph(null, Vector3D.Zero, Vector3I.Up);
+        }
+    }
+}
