56bc6e1800122f4e12716d88a6b006a0b0fda748
Karel Hovorka <git@karel-hovorka.eu>
2021-09-21 12:18:01 +0200

    Unit tested block polymorphism.

---
+++ JvmClient/src/jvmTest/kotlin/spaceEngineers/game/mockable/PolymorphicBlocksTest.kt
@@ -0,0 +1,39 @@
+package spaceEngineers.game.mockable
+
+import spaceEngineers.controller.SpaceEngineers
+import spaceEngineers.model.*
+import spaceEngineers.model.extensions.allBlocks
+import testhelp.MockOrRealGameTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+
+class PolymorphicBlocksTest : MockOrRealGameTest() {
+
+    @Test
+    fun smallGenerator() = testContext {
+        buildAndCheckType<FueledPowerProducer>("LargeBlockSmallGenerator")
+        buildAndCheckType<TerminalBlock>("LargeBlockSmallGenerator")
+        buildAndCheckType<FunctionalBlock>("LargeBlockSmallGenerator")
+    }
+
+    @Test
+    fun slideDoor() = testContext {
+        buildAndCheckType<DoorBase>("LargeBlockSlideDoor")
+        buildAndCheckType<TerminalBlock>("LargeBlockSlideDoor")
+        buildAndCheckType<FunctionalBlock>("LargeBlockSlideDoor")
+    }
+
+
+    private inline fun <reified T> SpaceEngineers.buildAndCheckType(blockType: String) {
+        val z = 1000
+        admin.character.teleport(Vec3(0, 0, z + 15), Vec3.FORWARD, Vec3.UP)
+        observer.observeNewBlocks()
+        admin.blocks.placeAt(blockType, Vec3(0, 0, z + 0), Vec3.FORWARD, Vec3.UP)
+        val block = observer.observeNewBlocks().allBlocks.first()
+        assertEquals(block.blockType, blockType)
+        assertTrue(block is T)
+        admin.blocks.remove(block.id)
+    }
+}
