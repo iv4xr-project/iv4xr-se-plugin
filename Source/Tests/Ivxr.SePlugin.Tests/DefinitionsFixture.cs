13f9addc93207dcf3dd4bea92c5301d65f46f5bb
Premysl Paska <premysl.paska@goodai.com>
2021-09-22 17:46:13 +0200

    Definitions fixture.

---
+++ Source/Tests/Ivxr.SePlugin.Tests/DefinitionsFixture.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using ParallelTasks;
+using Sandbox;
+using Sandbox.Definitions;
+using VRage;
+using VRage.FileSystem;
+using VRage.Game;
+using VRage.ObjectBuilders;
+
+
+namespace Ivxr.SeGameLib.Tests
+{
+    public class DefinitionsFixture : IDisposable
+    {
+        private MyObjectBuilder_Definitions m_definitions;
+
+        public MyObjectBuilder_Definitions Definitions
+        {
+            get => m_definitions;
+            set => m_definitions = value;
+        }
+
+        public Dictionary<string, MyCubeBlockDefinition> BlockDefinitions { get; set; }
+
+        public DefinitionsFixture()
+        {
+            const string contentPath = "..\\..\\..\\..\\..\\..\\se\\Sources\\SpaceEngineers\\Content";
+            MyFileSystem.Init(contentPath, Path.Combine(Directory.GetCurrentDirectory(), "data"));
+
+            MyXmlSerializerManager.RegisterSerializableBaseType(typeof(MyObjectBuilder_Base));
+
+            Assembly sectorTypeAssembly = typeof(MyObjectBuilder_Sector).Assembly;
+            RegisterAssembly(sectorTypeAssembly);
+
+            Assembly sandboxGameAssembly = typeof(MySandboxGame).Assembly;
+            RegisterAssembly(sandboxGameAssembly);
+
+            //Assembly objectBuildersAssembly = typeof(MyObjectBuilder_MotorAdvancedRotor).Assembly;
+            //RegisterAssembly(objectBuildersAssembly);
+
+            MyObjectBuilderSerializer.LoadSerializers();
+
+            //Parallel.Scheduler = new FakeTaskScheduler(); // TODO: is this necessary?
+            //List<Tuple<MyObjectBuilder_Definitions, string>> baseDefinitions = MyDefinitionManager.Static.GetSessionPreloadDefinitions();
+            //MyDefinitionManager.Static.PreloadDefinitions();
+
+            foreach (var file in Directory.GetFiles(Path.Combine(contentPath, "Data\\CubeBlocks")))
+            {
+                LoadCubeBlockDefinitionFile(file);
+            }                        
+
+            MySandboxGame.IsDedicated = true;
+        }
+
+        private void LoadCubeBlockDefinitionFile(string armor)
+        {
+            string cubeBlockDefinition = Path.Combine(Environment.CurrentDirectory, armor);
+            MyObjectBuilderSerializer.DeserializeXML<MyObjectBuilder_Definitions>(cubeBlockDefinition, out m_definitions);
+
+            if (BlockDefinitions == null)
+            {
+                BlockDefinitions = new Dictionary<string, MyCubeBlockDefinition>();
+            }
+
+            foreach (var builder in m_definitions.CubeBlocks)
+            {
+                var blockDefinition = new MyCubeBlockDefinition();
+                builder.Components = Array.Empty<MyObjectBuilder_CubeBlockDefinition.CubeBlockComponent>();
+                builder.PhysicalMaterial = "Default";
+                blockDefinition.Init(builder, MyModContext.BaseGame);
+
+                string key = blockDefinition.Id.TypeId + "+" + blockDefinition.Id.SubtypeName;
+                if (!BlockDefinitions.ContainsKey(key))
+                    BlockDefinitions.Add(key, blockDefinition);
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+
+        private static void RegisterAssembly(Assembly assembly)
+        {
+            MyObjectBuilderSerializer.RegisterFromAssembly(assembly);
+            MyObjectBuilderType.RegisterFromAssembly(assembly, true);
+            MyXmlSerializerManager.RegisterFromAssembly(assembly);
+            MyDefinitionManagerBase.RegisterTypesFromAssembly(assembly);
+        }
+    }     
+}
--- Source/Tests/Ivxr.SePlugin.Tests/NavGraphTests.cs
+++ Source/Tests/Ivxr.SePlugin.Tests/NavGraphTests.cs
@@ -1,6 +1,7 @@
 ﻿
 using Iv4xr.SePlugin.Control;
 using Iv4xr.SePlugin.Navigation;
+using Sandbox.Definitions;
 using Sandbox.Game.Entities;
 using Sandbox.Game.Entities.Character;
 using VRageMath;
@@ -8,17 +9,35 @@ using Xunit;
 
 namespace Ivxr.SeGameLib.Tests
 {
-    public class NavGraphTests
+    public class NavGraphTests : IClassFixture<DefinitionsFixture>
     {
+        private readonly DefinitionsFixture m_fixture;
+
+        public NavGraphTests(DefinitionsFixture definitionsFixture)
+        {
+            m_fixture = definitionsFixture;
+        }
+
+        [Fact]
+        public void DefinitionsAreNotEmpty()
+        {
+            var count = m_fixture.Definitions.CubeBlocks.Length;
+            Assert.NotEqual(0, count);
+        }
+        
         [Fact]
         public void CreatesGraph()
         {
             // We will not call methods that use LowLevelObserver
             var graphEditor = new NavGraphEditor(new LowLevelObserver(new GameSession()));
 
-            var grid = new MyCubeGrid();
+            //MyDefinitionManager.RegisterTypesFromAssembly();
+            var blockPlacer = new BlockPlacer();
+            blockPlacer.PlaceBlock("LargeBlockArmorBlock", Vector3.Zero, Vector3.Forward, Vector3.Up);
+            // var grid = new MyCubeGrid();
+            
             //grid.BuildGeneratedBlock()
-            var graph = graphEditor.CreateGraph(null, Vector3D.Zero, Vector3I.Up);
+            //var graph = graphEditor.CreateGraph(null, Vector3D.Zero, Vector3I.Up);
         }
     }
 }
