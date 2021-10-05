fb632f500062ed1e40433e3d26b3d73716778578
Premysl Paska <premysl.paska@goodai.com>
2021-10-05 11:57:55 +0200

    Navigation: initial version.
    .
    
    
    . ll observer
    
    
    . navigation WIP
    
    
    Add a helper class for step vectors.
    
    
    Navigation WIP 2
    
    
    . test WIP
    
    
    Extensions: add PlainVec3I.ToVector3I.

---
+++ Source/Ivxr.SePlugin/Navigation/FatNode.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Iv4xr.PluginLib.WorldModel;
+
+namespace Iv4xr.SePlugin.Navigation
+{
+    /// <summary>
+    /// Apart from position, stores also edges leading to connected vertices.
+    /// </summary>
+    public class FatNode
+    {
+        public readonly PlainVec3D Position;
+
+        public readonly List<FatNode> Neighbours = new List<FatNode>(capacity: 8);
+
+    }
+}
