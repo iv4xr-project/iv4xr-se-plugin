6e65a60702dfd02a2a5de0a3afed54e04159aafa
Premysl Paska <premysl.paska@goodai.com>
2021-10-05 12:33:21 +0200

    Convert also the unit test to CubeGrid. (PASSes!-)

--- Source/Tests/Ivxr.SePlugin.Tests/NavGraphTests.cs
+++ Source/Tests/Ivxr.SePlugin.Tests/NavGraphTests.cs
@@ -1,8 +1,9 @@
 ﻿
+using System.Collections.Generic;
+using Iv4xr.PluginLib.WorldModel;
+using Iv4xr.SePlugin;
 using Iv4xr.SePlugin.Control;
 using Iv4xr.SePlugin.Navigation;
-using Sandbox.Game.Entities;
-using Sandbox.Game.Entities.Character;
 using VRageMath;
 using Xunit;
 
@@ -10,15 +11,39 @@ namespace Ivxr.SeGameLib.Tests
 {
     public class NavGraphTests
     {
+        private CubeGrid CreateTestGrid()
+        {
+            var grid = new CubeGrid()
+            {
+                Blocks = new List<Block>()
+            };
+
+            for (var x = 0; x < 3; x++)
+            {
+                var gridPosition = new PlainVec3I(x, 0, 0);
+                
+                var block = new Block
+                {
+                    GridPosition = gridPosition,
+                    Position = new Vector3D(gridPosition.ToVector3I()).ToPlain()
+                };
+                
+                grid.Blocks.Add(block);
+            }
+
+            return grid;
+        }
+        
         [Fact]
         public void CreatesGraph()
         {
             // We will not call methods that use LowLevelObserver
             var graphEditor = new NavGraphEditor(new LowLevelObserver(new GameSession()));
 
-            var grid = new MyCubeGrid();
-            //grid.BuildGeneratedBlock()
-            var graph = graphEditor.CreateGraph(null, Vector3D.Zero, Vector3I.Up);
+            var grid = CreateTestGrid();
+            var graph = graphEditor.CreateGraph(grid, Vector3D.Zero, Vector3I.Up);
+            
+            Assert.Equal(3, graph.Nodes.Count);
         }
     }
 }
