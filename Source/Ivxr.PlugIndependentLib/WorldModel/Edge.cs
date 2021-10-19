ff4cf7dee8999189da1da2cdee48a43b5ee2a987
Premysl Paska <premysl.paska@goodai.com>
2021-10-19 11:14:51 +0200

    Add Edge and NavGraph classes.

---
+++ Source/Ivxr.PlugIndependentLib/WorldModel/Edge.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Iv4xr.PluginLib.WorldModel
+{
+
+    public class Edge
+    {
+        /// <summary>Smaller vertex index.</summary>
+        public int I;
+        
+        /// <summary>Greater vertex index.</summary>
+        public int J;
+
+        public Edge(int u, int v)
+        {
+            if (u == v)
+                throw new ArgumentException("Edge cannot exist between equal indices.");
+            if (u < 0 || v < 0)
+                throw new ArgumentException("Vertex index cannot be negative.");
+
+            I = Math.Min(u, v);
+            J = Math.Max(u, v);
+        }
+    }
+
+}
---
+++ Source/Ivxr.PlugIndependentLib/WorldModel/NavGraph.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Iv4xr.PluginLib.WorldModel
+{
+    public class NavGraph
+    {
+        // Vertices indexed from zero.
+        public List<PlainVec3D> VertexPositions;
+
+        public List<Edge> Edges;
+    }
+}
