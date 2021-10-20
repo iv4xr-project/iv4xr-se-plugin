c9ce81205bd074df3e8af88a27ced36d8eaf9423
Premysl Paska <premysl.paska@goodai.com>
2021-10-20 17:47:36 +0200

    . expose via an extension func!

--- Source/Ivxr.SePlugin/Navigation/FatNavGraph.cs
+++ Source/Ivxr.SePlugin/Navigation/FatNavGraph.cs
@@ -1,9 +1,18 @@
 ﻿using System.Collections.Generic;
+using Iv4xr.PluginLib.WorldModel;
 
 namespace Iv4xr.SePlugin.Navigation
 {
     public class FatNavGraph
     {
         public readonly List<FatNode> Nodes = new List<FatNode>();
+    }
+
+    public static class FatNavGraphExtensions
+    {
+        public static NavGraph ToNavGraph(this FatNavGraph fatNavGraph)
+        {
+            return FatNode.ConvertFatNavGraphToSlim(fatNavGraph);
+        }
     }
 }
