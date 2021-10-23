1e27bf9c620e60421f38cdc25b71ac3330d56dcd
Karel Hovorka <git@karel-hovorka.eu>
2021-10-23 18:25:29 +0200

    Component and inventory tests, fixed tests.

--- JvmClient/src/jvmTest/kotlin/spaceEngineers/game/mockable/CharacterObservationTest.kt
+++ JvmClient/src/jvmTest/kotlin/spaceEngineers/game/mockable/CharacterObservationTest.kt
@@ -3,16 +3,36 @@ package spaceEngineers.game.mockable
 import testhelp.MockOrRealGameTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
-class CharacterObservationTest : MockOrRealGameTest() {
+class CharacterObservationTest : MockOrRealGameTest(inMockResourcesDirectory("CharacterObservationTest.txt")) {
 
     @Test
-    fun characterStatsCreativeMode() = testContext {
+    fun characterFlagStats() = testContext {
+        val characterObservation = observer.observe()
+        assertTrue(characterObservation.helmetEnabled)
+        assertFalse(characterObservation.jetpackRunning)
+    }
+
+    @Test
+    fun characterBottomLeftStatsCreativeMode() = testContext {
         val characterObservation = observer.observe()
         assertEquals(1f, characterObservation.health)
         assertEquals(1f, characterObservation.oxygen)
         assertEquals(1f, characterObservation.hydrogen)
         assertEquals(1f, characterObservation.suitEnergy)
+    }
+
+    @Test
+    fun inventory() = testContext {
+        val inventory = observer.observe().inventory
+        assertEquals(0f, inventory.cargoPercentage)
+        assertEquals(94.8f, inventory.currentMass)
+        assertEquals(0.3138f, inventory.currentVolume)
+        assertEquals(9.223372E12f, inventory.maxMass)
+        assertEquals(9.223372E12f, inventory.maxVolume)
+        assertEquals(7, inventory.items.size)
     }
 
 }
