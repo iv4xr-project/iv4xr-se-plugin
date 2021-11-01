b3982594a9f16dfa2c0582be320de71bc083bc62
Premysl Paska <premysl.paska@goodai.com>
2021-11-01 17:12:43 +0100

    Add GridPosition to Block.
    Add GridPosition: update tests.

--- JvmClient/src/commonMain/kotlin/spaceEngineers/model/DataBlock.kt
+++ JvmClient/src/commonMain/kotlin/spaceEngineers/model/DataBlock.kt
@@ -25,6 +25,8 @@ data class DataBlock(
     override val minPosition: Vec3F,
     @SerialName("MaxPosition")
     override val maxPosition: Vec3F,
+    @SerialName("GridPosition")
+    override val gridPosition: Vec3I,
     @SerialName("Size")
     override val size: Vec3F,
     @SerialName("UseObjects")
