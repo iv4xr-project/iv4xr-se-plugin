8ac46cc5a9609a92e525aeaff0cfacbd8dc0e8a1
Premysl Paska <premysl.paska@goodai.com>
2021-11-02 17:43:25 +0100

    . one more test

--- JvmClient/src/jvmTest/kotlin/spaceEngineers/game/mockable/NavigationGraphTest.kt
+++ JvmClient/src/jvmTest/kotlin/spaceEngineers/game/mockable/NavigationGraphTest.kt
@@ -3,6 +3,7 @@ package spaceEngineers.game.mockable
 import testhelp.MockOrRealGameTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class NavigationGraphTest : MockOrRealGameTest(
     mockFile = inMockResourcesDirectory("NavigationGraphTest.txt")
@@ -13,6 +14,20 @@ class NavigationGraphTest : MockOrRealGameTest(
         observer.navigationGraph().let {
             assertEquals(40, it.nodes.size)
             assertEquals(67, it.edges.size)
+        }
+    }
+
+    @Test
+    fun navGraphConsistencyCheck() = testContext {
+        val navGraph = observer.navigationGraph()
+        val minId = navGraph.nodes.minOf { n -> n.id }
+        val maxId = navGraph.nodes.maxOf { n -> n.id }
+        assertTrue { minId < maxId }
+
+        navGraph.edges.forEach {
+            assertTrue { it.i < it.j }
+            assertTrue { it.i >= minId }
+            assertTrue { it.j <= maxId }
         }
     }
 }
