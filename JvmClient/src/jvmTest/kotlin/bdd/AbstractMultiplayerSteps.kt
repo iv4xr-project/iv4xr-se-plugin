c8d0af83317d8ff724e1cef2f186603635b99d46
Karel Hovorka <git@karel-hovorka.eu>
2022-02-07 11:45:58 +0100

    Easier multiple connection management.

--- JvmClient/src/jvmTest/kotlin/bdd/AbstractMultiplayerSteps.kt
+++ JvmClient/src/jvmTest/kotlin/bdd/AbstractMultiplayerSteps.kt
@@ -1,31 +1,22 @@
 package bdd
 
+import spaceEngineers.controller.MultipleConnectionManager
 import spaceEngineers.controller.SpaceEngineers
 
-abstract class AbstractMultiplayerSteps {
-    val connections: MutableMap<String, SpaceEngineers> = mutableMapOf()
+abstract class AbstractMultiplayerSteps(
+    val mcm: MultipleConnectionManager = MultipleConnectionManager()
+) {
 
-    val characterIdByName: MutableMap<String, String> = mutableMapOf()
-
-    var serverAddress = "localhost:27016"
-
-    val server: SpaceEngineers
-        get() = connections[MultiplayerSteps.SERVER] ?: error("No server connection")
-
-
-    fun connection(name: String, block: SpaceEngineers.() -> Unit) {
-        block(connection(name))
+    fun client(name: String, block: SpaceEngineers.() -> Unit) {
+        mcm.connection(name, block)
     }
 
-    fun connection(name: String): SpaceEngineers {
-        return connections[name] ?: error("No connection of name $name")
+    fun server(block: SpaceEngineers.() -> Unit) {
+        mcm.server(block)
     }
 
     fun getCharacterIdByName(name: String): String {
-        return characterIdByName.getOrPut(name) {
-            connection(name).observer.observe().id
-        }
+        return mcm.getCharacterIdByName(name)
     }
-
 
 }
