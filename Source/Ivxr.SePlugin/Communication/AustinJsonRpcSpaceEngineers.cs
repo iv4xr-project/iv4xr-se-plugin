a0fdf18a1fe7edf009d8241c4fa940ba15a329bf
Karel Hovorka <git@karel-hovorka.eu>
2022-02-10 12:17:33 +0100

    More automatic binding of services.  - Annotations not required.  - Possible to bind any class/interface, it's declared public methods are bound.  - Possible to add prefix to method names.

--- Source/Ivxr.SePlugin/Communication/AustinJsonRpcSpaceEngineers.cs
+++ Source/Ivxr.SePlugin/Communication/AustinJsonRpcSpaceEngineers.cs
@@ -1,9 +1,5 @@
-﻿using System.Collections.Generic;
-using AustinHarris.JsonRpc;
-using Iv4xr.PluginLib.Control;
+﻿using AustinHarris.JsonRpc;
 using Iv4xr.SpaceEngineers;
-using Iv4xr.SpaceEngineers.Navigation;
-using Iv4xr.SpaceEngineers.WorldModel;
 
 namespace Iv4xr.SePlugin.Communication
 {
@@ -15,270 +11,30 @@ namespace Iv4xr.SePlugin.Communication
         {
             m_se = se;
         }
+        
+        private static void BindService<TType>(string sessionID, TType instance, string methodPrefix)
+        {
+            AustinUtil.BindService<TType>(sessionID, instance, methodPrefix);
+        }
 
         public void Bind()
         {
-            ServiceBinder.BindService(Handler.DefaultSessionId(), this);
-        }
+            var sessionId = Handler.DefaultSessionId();
+            
+            BindService<ICharacterController>(sessionId, m_se.Character, "Character.");
+            BindService<IItems>(sessionId, m_se.Items, "Items.");
+            BindService<IObserver>(sessionId, m_se.Observer, "Observer.");
+            BindService<ISessionController>(sessionId, m_se.Session, "Session.");
+            BindService<IDefinitions>(sessionId, m_se.Definitions, "Definitions.");
+            BindService<IBlocks>(sessionId, m_se.Blocks, "Blocks.");
 
-        [JsonRpcMethod("Character.MoveAndRotate")]
-        CharacterObservation MoveAndRotate(PlainVec3D movement, PlainVec2F rotation3, float roll = 0, int ticks = 1)
-        {
-            return m_se.Character.MoveAndRotate(movement, rotation3, roll, ticks);
-        }
+            BindService<ISpaceEngineersAdmin>(sessionId, m_se.Admin, "Admin.");
+            BindService<IBlocksAdmin>(sessionId, m_se.Admin.Blocks, "Admin.Blocks.");
+            BindService<ICharacterAdmin>(sessionId, m_se.Admin.Character, "Admin.Character.");
+            BindService<IObserverAdmin>(sessionId, m_se.Admin.Observer, "Admin.Observer.");
 
-        [JsonRpcMethod("Admin.Character.Teleport")]
-        CharacterObservation Teleport(PlainVec3D position, PlainVec3D? orientationForward = null, PlainVec3D? orientationUp = null)
-        {
-            return m_se.Admin.Character.Teleport(position, orientationForward, orientationUp);
-        }
-
-        [JsonRpcMethod("Admin.Character.Use")]
-        void Use(string blockId, int functionIndex, int action)
-        {
-            m_se.Admin.Character.Use(blockId, functionIndex, action);
-        }
-
-        [JsonRpcMethod("Admin.Character.Create")]
-        CharacterObservation Create(string name, PlainVec3D position, PlainVec3D orientationForward, PlainVec3D orientationUp)
-        {
-            return m_se.Admin.Character.Create(name, position, orientationForward, orientationUp);
-        }
-        
-        [JsonRpcMethod("Admin.Character.Switch")]
-        void Switch(string id)
-        {
-            m_se.Admin.Character.Switch(id);
-        }
-        
-        [JsonRpcMethod("Admin.Character.Remove")]
-        void RemoveCharacter(string id)
-        {
-            m_se.Admin.Character.Remove(id);
-        }
-
-        [JsonRpcMethod("Admin.Observer.ObserveCharacters")]
-        List<CharacterObservation> AdminObserveCharacters()
-        {
-            return m_se.Admin.Observer.ObserveCharacters();
-        }
-
-        [JsonRpcMethod("Character.TurnOnJetpack")]
-        CharacterObservation TurnOnJetpack()
-        {
-            return m_se.Character.TurnOnJetpack();
-        }
-
-        [JsonRpcMethod("Character.TurnOffJetpack")]
-        CharacterObservation TurnOffJetpack()
-        {
-            return m_se.Character.TurnOffJetpack();
-        }
-        
-        [JsonRpcMethod("Character.SwitchHelmet")]
-        CharacterObservation SwitchHelmet()
-        {
-            return m_se.Character.SwitchHelmet();
-        }
-        
-        [JsonRpcMethod("Character.Use")]
-        void Use()
-        {
-            m_se.Character.Use();
-        }
-        
-        [JsonRpcMethod("Screens.Medicals.MedicalRooms")]
-        List<MedicalRoom> MedicalRooms()
-        {
-            return m_se.Screens.Medicals.MedicalRooms();
-        }
-        
-        [JsonRpcMethod("Screens.Medicals.Respawn")]
-        void Respawn(int roomIndex)
-        {
-            m_se.Screens.Medicals.Respawn(roomIndex);
-        }
-        
-        [JsonRpcMethod("Screens.Medicals.Factions")]
-        List<Faction> Factions()
-        {
-            return m_se.Screens.Medicals.Factions();
-        }
-        
-        [JsonRpcMethod("Screens.Medicals.ChooseFaction")]
-        void ChooseFaction(int factionIndex)
-        {
-            m_se.Screens.Medicals.ChooseFaction(factionIndex);
-        }
-
-        [JsonRpcMethod("Session.LoadScenario")]
-        void LoadScenario(string scenarioPath)
-        {
-            m_se.Session.LoadScenario(scenarioPath);
-        }
-        
-        [JsonRpcMethod("Session.Connect")]
-        void Connect(string address)
-        {
-            m_se.Session.Connect(address);
-        }
-        
-        [JsonRpcMethod("Session.Disconnect")]
-        void Disconnect()
-        {
-            m_se.Session.Disconnect();
-        }
-        
-        [JsonRpcMethod("Session.ExitGame")]
-        void ExitGame()
-        {
-            m_se.Session.ExitGame();
-        }
-        
-        [JsonRpcMethod("Screens.FocusedScreen")]
-        string FocusedScreen()
-        {
-            return m_se.Screens.FocusedScreen();
-        }
-        
-        [JsonRpcMethod("Screens.WaitUntilTheGameLoaded")]
-        void WaitUntilTheGameLoaded()
-        {
-            m_se.Screens.WaitUntilTheGameLoaded();
-        }
-
-        [JsonRpcMethod("Observer.Observe")]
-        CharacterObservation Observe()
-        {
-            return m_se.Observer.Observe();
-        }
-
-        [JsonRpcMethod("Observer.ObserveBlocks")]
-        Observation ObserveBlocks()
-        {
-            return m_se.Observer.ObserveBlocks();
-        }
-
-        [JsonRpcMethod("Observer.ObserveNewBlocks")]
-        Observation ObserveNewBlocks()
-        {
-            return m_se.Observer.ObserveNewBlocks();
-        }
-        
-        [JsonRpcMethod("Observer.ObserveCharacters")]
-        List<CharacterObservation> ObserveCharacters()
-        {
-            return m_se.Observer.ObserveCharacters();
-        }
-
-        
-        [JsonRpcMethod("Observer.SwitchCamera")]
-        void SwitchCamera()
-        {
-            m_se.Observer.SwitchCamera();
-        }
-
-        [JsonRpcMethod("Observer.NavigationGraph")]
-        NavGraph GetNavigationGraph()
-        {
-            return m_se.Observer.NavigationGraph();
-        }
-
-        [JsonRpcMethod("Observer.TakeScreenshot")]
-        void TakeScreenshot(string absolutePath)
-        {
-            m_se.Observer.TakeScreenshot(absolutePath);
-        }
-
-        [JsonRpcMethod("Definitions.BlockDefinitions")]
-        List<BlockDefinition> BlockDefinitions()
-        {
-            return m_se.Definitions.BlockDefinitions();
-        }
-
-        [JsonRpcMethod("Definitions.AllDefinitions")]
-        List<DefinitionBase> AllDefinitions()
-        {
-            return m_se.Definitions.AllDefinitions();
-        }
-        
-        [JsonRpcMethod("Definitions.BlockHierarchy")]
-        Dictionary<string, string> BlockHierarchy()
-        {
-            return m_se.Definitions.BlockHierarchy();
-        }
-        
-        [JsonRpcMethod("Definitions.BlockDefinitionHierarchy")]
-        Dictionary<string, string> BlockDefinitionHierarchy()
-        {
-            return m_se.Definitions.BlockDefinitionHierarchy();
-        }
-
-        [JsonRpcMethod("Blocks.Place")]
-        void Place()
-        {
-            m_se.Blocks.Place();
-        }
-
-        [JsonRpcMethod("Admin.Blocks.PlaceAt")]
-        string PlaceAt(DefinitionId blockDefinitionId, PlainVec3D position, PlainVec3D orientationForward, PlainVec3D orientationUp)
-        {
-            return m_se.Admin.Blocks.PlaceAt(blockDefinitionId, position, orientationForward, orientationUp);
-        }
-
-        [JsonRpcMethod("Admin.Blocks.PlaceInGrid")]
-        string PlaceInGrid(DefinitionId blockDefinitionId, string gridId, PlainVec3I minPosition, PlainVec3I orientationForward,
-            PlainVec3I orientationUp)
-        {
-            return m_se.Admin.Blocks.PlaceInGrid(blockDefinitionId, gridId, minPosition, orientationForward, orientationUp);
-        }
-        
-        [JsonRpcMethod("Admin.Blocks.Remove")]
-        void Remove(string blockId)
-        {
-            m_se.Admin.Blocks.Remove(blockId);
-        }
-        
-        [JsonRpcMethod("Admin.Blocks.SetIntegrity")]
-        void SetIntegrity(string blockId, float integrity)
-        {
-            m_se.Admin.Blocks.SetIntegrity(blockId, integrity);
-        }
-
-        [JsonRpcMethod("Admin.SetFrameLimitEnabled")]
-        void SetFrameLimitEnabled(bool enabled)
-        {
-            m_se.Admin.SetFrameLimitEnabled(enabled);
-        }
-
-        [JsonRpcMethod("Character.BeginUsingTool")]
-        void BeginUsingTool()
-        {
-            m_se.Character.BeginUsingTool();
-        }
-
-        [JsonRpcMethod("Character.EndUsingTool")]
-        void EndUsingTool()
-        {
-            m_se.Character.EndUsingTool();
-        }
-
-        [JsonRpcMethod("Items.Equip")]
-        void Equip(ToolbarLocation toolbarLocation)
-        {
-            m_se.Items.Equip(toolbarLocation);
-        }
-
-        [JsonRpcMethod("Items.SetToolbarItem")]
-        void SetToolbarItem(string name, ToolbarLocation toolbarLocation)
-        {
-            m_se.Items.SetToolbarItem(name, toolbarLocation);
-        }
-
-        [JsonRpcMethod("Items.Toolbar")]
-        Toolbar Toolbar()
-        {
-            return m_se.Items.Toolbar();
+            BindService<IScreens>(sessionId, m_se.Screens, "Screens.");
+            BindService<IMedicals>(sessionId, m_se.Screens.Medicals, "Screens.Medicals.");
         }
     }
 }
