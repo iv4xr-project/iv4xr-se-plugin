02d591d2d79723aa694dff86b846f4eb6d916e8f
Karel Hovorka <git@karel-hovorka.eu>
2022-03-28 23:45:50 +0200

    Call roles, basic runtime checks.

---
+++ Source/Ivxr.SpaceEngineers/ITestAdmin.cs
@@ -0,0 +1,16 @@
+﻿using static Iv4xr.SpaceEngineers.Role;
+
+namespace Iv4xr.SpaceEngineers
+{
+    public interface ITestAdmin
+    {
+        [Role(Admin)]
+        void AdminOnly();
+
+        [Role(Game)]
+        void GameOnly();
+
+        [Role(Observer)]
+        void ObserverOnly();
+    }
+}
