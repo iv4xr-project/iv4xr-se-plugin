26744af014fcf882cf3e4d862c2562fa91780c4a
Karel Hovorka <git@karel-hovorka.eu>
2022-04-05 13:44:55 +0200

    Maze generator + one instance of maze generated as a save.

---
+++ JvmClient/src/commonMain/kotlin/spaceEngineers/util/generator/maze/Position.kt
@@ -0,0 +1,42 @@
+package spaceEngineers.util.generator.maze
+
+operator fun Position.plus(direction: Direction): DirectedPosition {
+    return DirectedPosition(
+        position = Position(this.x + direction.vector.x, this.y + direction.vector.y),
+        direction = direction
+    )
+}
+
+
+operator fun Position.plus(position: Position): Position {
+    return Position(this.x + position.x, this.y + position.y)
+}
+
+data class Position(val x: Int, val y: Int) {
+
+    fun down(): DirectedPosition {
+        return this + Direction.DOWN
+    }
+
+
+    fun up(): DirectedPosition {
+        return this + Direction.UP
+    }
+
+    fun left(): DirectedPosition {
+        return this + Direction.LEFT
+    }
+
+    fun right(): DirectedPosition {
+        return this + Direction.RIGHT
+    }
+
+    fun surround(): Array<DirectedPosition> {
+        return arrayOf(
+            up(),
+            down(),
+            left(),
+            right()
+        )
+    }
+}
