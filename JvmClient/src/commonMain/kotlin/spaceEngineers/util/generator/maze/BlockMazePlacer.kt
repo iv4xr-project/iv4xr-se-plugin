e95077d01fa3db244822039e0bfb253f7abb600b
Karel Hovorka <git@karel-hovorka.eu>
2022-04-06 23:21:58 +0200

    Generate maze as single grid.

--- JvmClient/src/commonMain/kotlin/spaceEngineers/util/generator/maze/BlockMazePlacer.kt
+++ JvmClient/src/commonMain/kotlin/spaceEngineers/util/generator/maze/BlockMazePlacer.kt
@@ -4,6 +4,7 @@ import spaceEngineers.controller.SpaceEngineers
 import spaceEngineers.model.DefinitionId
 import spaceEngineers.model.LARGE_BLOCK_CUBE_SIDE_SIZE
 import spaceEngineers.model.Vec3F
+import spaceEngineers.model.Vec3I
 
 
 class BlockMazePlacer(
@@ -37,6 +38,45 @@ class BlockMazePlacer(
                             orientationUp = Vec3F.UP,
                         )
 
+                    }
+                }
+            }
+        }
+    }
+
+    fun generateAsSingleGrid() {
+        val z = 0
+        var gridId: String? = null
+        for (x in 0..maze.width) {
+            for (y in 0..maze.height) {
+                if (gridId == null) {
+                    spaceEngineers.admin.blocks.placeAt(
+                        blockDefinitionId = bottomBlockDefinitionId,
+                        position = Vec3F(x, y, z) * LARGE_BLOCK_CUBE_SIDE_SIZE,
+                        orientationForward = Vec3F.FORWARD,
+                        orientationUp = Vec3F.UP,
+                    )
+                    gridId = spaceEngineers.observer.observeBlocks().grids.first().id
+                } else {
+                    spaceEngineers.admin.blocks.placeInGrid(
+                        gridId = gridId,
+                        blockDefinitionId = bottomBlockDefinitionId,
+                        orientationUp = Vec3I.UP,
+                        orientationForward = Vec3I.FORWARD,
+                        minPosition = Vec3I(x, y, z)
+                    )
+                }
+
+                val cell = maze[x, y]
+                if (!cell.canMove) {
+                    repeat(1) {
+                        spaceEngineers.admin.blocks.placeInGrid(
+                            gridId = gridId,
+                            blockDefinitionId = wallBlockDefinitionId,
+                            orientationUp = Vec3I.UP,
+                            orientationForward = Vec3I.FORWARD,
+                            minPosition = Vec3I(x, y, it + 1)
+                        )
                     }
                 }
             }
