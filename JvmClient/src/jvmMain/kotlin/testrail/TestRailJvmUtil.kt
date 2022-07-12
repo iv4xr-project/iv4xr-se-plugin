f64bbccf8ee0f8a95bc4d506804f5f80f5d16267
Karel Hovorka <git@karel-hovorka.eu>
2022-07-12 20:32:07 +0200

    Downloading test cases in a directory tree structure.

--- JvmClient/src/jvmMain/kotlin/testrail/TestRailJvmUtil.kt
+++ JvmClient/src/jvmMain/kotlin/testrail/TestRailJvmUtil.kt
@@ -1,21 +1,46 @@
 package testrail
 
 import spaceEngineers.util.extractTo
+import testrail.model.Case
+import testrail.model.Section
 import java.io.ByteArrayInputStream
 import java.io.File
 import java.util.zip.ZipInputStream
 
 suspend fun TestRailClient.downloadCase(
     caseId: Long,
+    projectId: Long = 3,
     outputDirectory: File = File("./src/jvmTest/resources/features/testrail/"),
     overwrite: Boolean = false,
 ) {
     val case = getCase(caseId)
     //val attachmentsForCase = getAttachmentsForCase(caseId)
-    val outputFile = File(outputDirectory, "C$caseId.feature")
+
+    val ignoredSections = setOf(49384L, 49385L, 49386L)
+
+    val sections = getSections(case, projectId).filter { it.id !in ignoredSections }.joinToString(separator = "/") {
+        "${it.name}-${it.id}"
+    }
+    val dir = File(outputDirectory, sections).apply { mkdirs() }
+    val outputFile = File(dir, "C$caseId.feature")
     if (overwrite || !outputFile.exists()) {
         outputFile.writeText(case.featureContent())
     }
+}
+
+suspend fun TestRailClient.getSections(case: Case, projectId: Long): List<Section> {
+    val sectionsById = getSections(projectId = projectId, suiteId = case.suite_id).sections.associateBy {
+        it.id
+    }
+    val sections = mutableListOf<Section>()
+    var section: Section? = sectionsById[case.section_id]
+    do {
+        section?.let {
+            sections.add(it)
+        }
+        section = sectionsById[section?.parent_id]
+    } while (section != null)
+    return sections.asReversed()
 }
 
 suspend fun TestRailClient.downloadMaps(
