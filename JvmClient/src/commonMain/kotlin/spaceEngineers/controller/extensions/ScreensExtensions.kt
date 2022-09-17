50339247da8e41515d6e9f15fe6b79625d2c3e41
Karel Hovorka <git@karel-hovorka.eu>
2022-09-17 15:20:15 +0200

    Using Duration for time intervals rather than "ms" Long variables.

--- JvmClient/src/commonMain/kotlin/spaceEngineers/controller/extensions/ScreensExtensions.kt
+++ JvmClient/src/commonMain/kotlin/spaceEngineers/controller/extensions/ScreensExtensions.kt
@@ -4,9 +4,21 @@ import spaceEngineers.controller.Screens
 import spaceEngineers.model.ScreenName
 import spaceEngineers.model.toScreenName
 import spaceEngineers.util.whileWithTimeout
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
 
 suspend fun Screens.waitForScreen(timeoutMs: Long = 60_000, singleDelay: Long = 500, screenName: ScreenName) {
     whileWithTimeout(timeoutMs, singleDelay) {
+        typedFocusedScreen() != screenName
+    }
+}
+
+suspend fun Screens.waitForScreen(
+    timeout: Duration = 60_000.milliseconds,
+    retryInterval: Duration = 500.milliseconds,
+    screenName: ScreenName
+) {
+    whileWithTimeout(timeout, retryInterval) {
         typedFocusedScreen() != screenName
     }
 }
--- JvmClient/src/commonMain/kotlin/spaceEngineers/util/CoroutineUtil.kt
+++ JvmClient/src/commonMain/kotlin/spaceEngineers/util/CoroutineUtil.kt
@@ -2,8 +2,23 @@ package spaceEngineers.util
 
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.withTimeout
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
 
-suspend fun whileWithTimeout(timeout: Long = 10_000L, waitDelay: Long = 100L, condition: () -> Boolean) {
+suspend fun whileWithTimeout(
+    timeout: Duration = 10_000.milliseconds,
+    waitDelay: Duration = 100.milliseconds,
+    condition: () -> Boolean
+) {
+    withTimeout(timeout) {
+        while (condition()) {
+            delay(waitDelay)
+        }
+    }
+}
+
+@Deprecated("Use method that passes kotlin.time.Duration")
+suspend fun whileWithTimeout(timeout: Long, waitDelay: Long = 100L, condition: () -> Boolean) {
     withTimeout(timeout) {
         while (condition()) {
             delay(waitDelay)
