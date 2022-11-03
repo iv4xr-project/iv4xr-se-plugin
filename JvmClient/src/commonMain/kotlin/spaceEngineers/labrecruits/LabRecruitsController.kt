72f47a6be5f2bdaffaef133e8e182a2fd237f0ba
Karel Hovorka <git@karel-hovorka.eu>
2022-11-03 12:56:18 +0100

    Simple observation example.

--- JvmClient/src/commonMain/kotlin/spaceEngineers/labrecruits/LabRecruitsController.kt
+++ JvmClient/src/commonMain/kotlin/spaceEngineers/labrecruits/LabRecruitsController.kt
@@ -1,21 +1,53 @@
 package spaceEngineers.labrecruits
 
 import spaceEngineers.controller.ExtendedSpaceEngineers
+import spaceEngineers.model.CharacterObservation
 import spaceEngineers.model.DoorBase
 import spaceEngineers.model.TerminalBlock
+import spaceEngineers.model.extensions.allBlocks
 import spaceEngineers.model.extensions.blockByCustomName
+import spaceEngineers.model.typing.DefinitionIds
 
 class LabRecruitsController(
     val spaceEngineers: ExtendedSpaceEngineers,
     val maximumButtonReachDistance: Float = 3.5f,
 ) {
 
-    suspend fun <T> se(block: suspend ExtendedSpaceEngineers.() -> T): T {
+    private suspend fun <T> se(block: suspend ExtendedSpaceEngineers.() -> T): T {
         return block(spaceEngineers)
     }
 
-    fun <T> seSync(block: ExtendedSpaceEngineers.() -> T): T {
+    private fun <T> seSync(block: ExtendedSpaceEngineers.() -> T): T {
         return block(spaceEngineers)
+    }
+
+    data class SimpleLabRecruitsObservation(
+        val door: List<DoorBase>,
+        val buttons: List<TerminalBlock>,
+        val character: CharacterObservation,
+    ) {
+        override fun toString(): String {
+            return """
+                ${door.map { it.customName }}
+                ${buttons.map { it.customName }}
+                ${character.position}
+            """.trimIndent()
+        }
+    }
+
+    fun observeActiveObjects() = seSync {
+        val navGraph = observer.navigationGraph(observer.observeBlocks().grids.first().id)
+        val allNodePositions = navGraph.nodes.map { it.data }
+        val interestingBlocks = observer.observeBlocks().allBlocks.filterIsInstance<TerminalBlock>().filter { block ->
+            allNodePositions.any { block.position.distanceTo(it) < 3f }
+        }
+        val door = interestingBlocks.filterIsInstance<DoorBase>()
+        val buttons = interestingBlocks.filter { it.definitionId == DefinitionIds.ButtonPanel.LargeSciFiButtonTerminal }
+        SimpleLabRecruitsObservation(
+            door = door,
+            buttons = buttons,
+            character = observer.observe(),
+        )
     }
 
 
