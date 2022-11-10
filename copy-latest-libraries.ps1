96578b55efa85959a92080c09c45ec48bf7a47c4
Karel Hovorka <git@karel-hovorka.eu>
2022-11-10 12:53:15 +0100

    Release publish script update.
    Release publish script is now releasing non-draft and non-prelease snapshot. Now the newest release can be targeted as the latest and automatically copied to the game using copy-latest-libraries script.

---
+++ copy-latest-libraries.ps1
@@ -0,0 +1,3 @@
+wget https://github.com/iv4xr-project/iv4xr-se-plugin/releases/latest/download/Ivxr.SePlugin.dll -OutFile "C:\Program Files (x86)\Steam\steamapps\common\SpaceEngineers\Bin64\Ivxr.SePlugin.dll"
+wget https://github.com/iv4xr-project/iv4xr-se-plugin/releases/latest/download/Ivxr.SpaceEngineers.dll -OutFile "C:\Program Files (x86)\Steam\steamapps\common\SpaceEngineers\Bin64\Ivxr.SpaceEngineers.dll"
+wget https://github.com/iv4xr-project/iv4xr-se-plugin/releases/latest/download/Ivxr.PlugIndependentLib.dll -OutFile "C:\Program Files (x86)\Steam\steamapps\common\SpaceEngineers\Bin64\Ivxr.PlugIndependentLib.dll"
--- publish-snapshot.ps1
+++ publish-snapshot.ps1
@@ -1,5 +1,5 @@
 $hash = (git rev-parse --short HEAD 2>&1)
-gh release create -d --generate-notes -F snapshot-release-notes.md  -t "SE Plugin snapshot release $hash" -p "SNAPSHOT-$hash" `
+gh release create --generate-notes -F snapshot-release-notes.md  -t "SE Plugin snapshot release $hash"  "SNAPSHOT-$hash" `
 .\Source\Ivxr.SePlugin\bin\Debug\netstandard2.0\Ivxr.PlugIndependentLib.dll `
 .\Source\Ivxr.SePlugin\bin\Debug\netstandard2.0\Ivxr.SePlugin.dll `
 .\Source\Ivxr.SePlugin\bin\Debug\netstandard2.0\Ivxr.SpaceEngineers.dll `
