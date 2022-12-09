478b454021731b1c8d9900d394d5bf61889593a7
Karel Hovorka <git@karel-hovorka.eu>
2022-12-09 14:03:44 +0100

    Cleanup for better coverage.

--- JvmClient/src/jvmMain/kotlin/spaceEngineers/controller/connection/ConnectionSetup.kt
+++ JvmClient/src/jvmMain/kotlin/spaceEngineers/controller/connection/ConnectionSetup.kt
@@ -1,23 +1,8 @@
 package spaceEngineers.controller.connection
 
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
-import spaceEngineers.controller.connection.ConnectionSetupBuilder.Companion.LOCALHOST
-import spaceEngineers.controller.connection.ConnectionSetupBuilder.Companion.LOCAL_SOURCES_DIR_KAREL
-import spaceEngineers.controller.connection.ConnectionSetupBuilder.Companion.REMOTE_DESKTOP
+import spaceEngineers.controller.json
 import java.io.File
-
-fun main(args: Array<String>) {
-    val CONNECTION_SETUP_DIR = File("src/jvmTest/resources/connection-setup/")
-    val json = Json {
-        prettyPrint = true
-    }
-    CONNECTION_SETUP_DIR.mkdirs()
-    ConnectionSetup.byName.forEach {
-        File(CONNECTION_SETUP_DIR, it.key).writeText(json.encodeToString(it.value))
-    }
-}
 
 @Serializable
 data class ConnectionSetup(
@@ -55,56 +40,6 @@ data class ConnectionSetup(
     }
 
     companion object {
-        val OFFLINE_STEAM = ConnectionSetupBuilder(gameVersion = GameVersion.STEAM).setServer(
-            AppType.GAME, address = LOCALHOST,
-        ).build()
-
-        val OFFLINE_DEV_KAREL = ConnectionSetupBuilder(
-            gameVersion = GameVersion.DEV,
-            localSourcesDirectory = LOCAL_SOURCES_DIR_KAREL
-        ).setServer(
-            AppType.GAME, address = LOCALHOST,
-        ).build()
-
-        val SINGLE_COMPUTER_DEDICATED_DEV_KAREL = ConnectionSetupBuilder(
-            gameVersion = GameVersion.DEV,
-            localSourcesDirectory = LOCAL_SOURCES_DIR_KAREL
-        ).setServer(
-            AppType.DEDICATED, address = LOCALHOST,
-        ).addMainClient(LOCALHOST).build()
-
-        val SINGLE_COMPUTER_DEDICATED_STEAM = ConnectionSetupBuilder(gameVersion = GameVersion.STEAM).setServer(
-            AppType.DEDICATED, address = LOCALHOST,
-        ).addMainClient(LOCALHOST).build()
-
-        val DOUBLE_PC_DEDICATED_DEV_KAREL =
-            ConnectionSetupBuilder(gameVersion = GameVersion.DEV, localSourcesDirectory = LOCAL_SOURCES_DIR_KAREL)
-                .setServer(AppType.DEDICATED, address = LOCALHOST)
-                .addMainClient(LOCALHOST).addClient("10.0.0.33").build()
-
-        val DOUBLE_PC_LOBBY_STEAM = ConnectionSetupBuilder(gameVersion = GameVersion.STEAM)
-            .setServer(AppType.GAME, address = LOCALHOST)
-            .addClient("10.0.0.33").build()
-
-        val DOUBLE_PC_LOBBY_STEAM_REMOTE_DESKTOP = ConnectionSetupBuilder(gameVersion = GameVersion.STEAM)
-            .setServer(AppType.GAME, address = REMOTE_DESKTOP)
-            .addClient(LOCALHOST).build()
-
-        val DOUBLE_PC_LOBBY_DEV_KAREL =
-            ConnectionSetupBuilder(gameVersion = GameVersion.DEV, localSourcesDirectory = LOCAL_SOURCES_DIR_KAREL)
-                .setServer(AppType.GAME, address = LOCALHOST)
-                .addClient("10.11.12.10").build()
-
-        val byName = mapOf(
-            "OFFLINE_STEAM.json" to OFFLINE_STEAM,
-            "OFFLINE_DEV_KAREL.json" to OFFLINE_DEV_KAREL,
-            "SINGLE_COMPUTER_DEDICATED_STEAM" to SINGLE_COMPUTER_DEDICATED_STEAM,
-            "SINGLE_COMPUTER_DEDICATED_DEV_KAREL" to SINGLE_COMPUTER_DEDICATED_DEV_KAREL,
-            "DOUBLE_PC_LOBBY_STEAM" to DOUBLE_PC_LOBBY_STEAM,
-        )
-
-        private val json: Json = Json {
-        }
 
         val CONNECTION_SETUP_DIR = "src/main/resources/connection-setup/"
 
@@ -112,7 +47,10 @@ data class ConnectionSetup(
             return json.decodeFromString(serializer(), file.readText())
         }
 
-        fun loadConfigFromFile(name: String = "config.json", directory: String = CONNECTION_SETUP_DIR): ConnectionSetup {
+        fun loadConfigFromFile(
+            name: String = "config.json",
+            directory: String = CONNECTION_SETUP_DIR
+        ): ConnectionSetup {
             return loadConfigFromFile(File(directory, name))
         }
     }
