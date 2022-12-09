af5041622b90c925ff9c3281aacc9d321eaf6e63
Karel Hovorka <hovi6337@gmail.com>
2022-12-09 17:12:13 +0100

    Piston velocity API.

---
+++ Source/Ivxr.SePlugin/Control/Screen/BlockAdmin/PistonBaseAdmin.cs
@@ -0,0 +1,20 @@
+﻿using Iv4xr.SpaceEngineers;
+using Iv4xr.SpaceEngineers.WorldModel;
+using Sandbox.Game.Entities.Blocks;
+using Sandbox.Game.Entities.Cube;
+
+namespace Iv4xr.SePlugin.Control.Screen.BlockAdmin
+{
+    public class PistonBaseAdmin : AbstractBlockAdmin<MyPistonBase>, IPistonBaseAdmin
+    {
+        public PistonBaseAdmin(IGameSession session, LowLevelObserver observer) : base(session, observer)
+        {
+        }
+
+        public void SetVelocity(string blockId, float velocity)
+        {
+            BlockById(blockId).Velocity.Value = velocity;
+        }
+
+    }
+}
