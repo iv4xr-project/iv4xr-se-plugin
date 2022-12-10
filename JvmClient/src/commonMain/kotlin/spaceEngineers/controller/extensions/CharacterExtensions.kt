e131383b4f0e7e403699aa5482b28bf1afe6bf5e
Karel Hovorka <hovi6337@gmail.com>
2022-12-10 20:00:16 +0100

    Cleanup.

--- JvmClient/src/commonMain/kotlin/spaceEngineers/controller/extensions/CharacterExtensions.kt
+++ JvmClient/src/commonMain/kotlin/spaceEngineers/controller/extensions/CharacterExtensions.kt
@@ -2,54 +2,9 @@ package spaceEngineers.controller.extensions
 
 import kotlinx.coroutines.withTimeout
 import kotlinx.coroutines.yield
-import spaceEngineers.controller.Character
 import spaceEngineers.controller.SpaceEngineers
-import spaceEngineers.model.CharacterMovementType
-import spaceEngineers.model.CharacterObservation
 import spaceEngineers.model.Vec2F
 import spaceEngineers.model.Vec3F
-import spaceEngineers.model.extensions.normalizeAsMovement
-
-fun Character.moveForward(characterMovementType: CharacterMovementType): CharacterObservation {
-    return moveAndRotate(movement = Vec3F.FORWARD.normalizeAsMovement(characterMovementType))
-}
-
-fun Character.moveBackward(characterMovementType: CharacterMovementType): CharacterObservation {
-    return moveAndRotate(movement = Vec3F.BACKWARD.normalizeAsMovement(characterMovementType))
-}
-
-suspend fun SpaceEngineers.blockingMoveByDistance(
-    distance: Float,
-    startPosition: Vec3F,
-    timeoutMs: Long = 20000,
-    orientation: Vec3F,
-    characterMovementType: CharacterMovementType = CharacterMovementType.RUN,
-) {
-    withTimeout(timeoutMs) {
-        while (observer.observe().position.distanceTo(startPosition) < distance) {
-            character.moveAndRotate(movement = orientation.normalizeAsMovement(characterMovementType))
-            yield()
-        }
-    }
-}
-
-suspend fun SpaceEngineers.blockingMoveForwardByDistance(
-    distance: Float,
-    startPosition: Vec3F,
-    timeoutMs: Long = 20000,
-    characterMovementType: CharacterMovementType = CharacterMovementType.RUN,
-) {
-    blockingMoveByDistance(distance = distance, startPosition = startPosition, timeoutMs = timeoutMs, characterMovementType = characterMovementType, orientation = Vec3F.FORWARD)
-}
-
-suspend fun SpaceEngineers.blockingMoveBackwardsByDistance(
-    distance: Float,
-    startPosition: Vec3F,
-    timeoutMs: Long = 20000,
-    characterMovementType: CharacterMovementType = CharacterMovementType.RUN,
-) {
-    blockingMoveByDistance(distance = distance, startPosition = startPosition, timeoutMs = timeoutMs, characterMovementType = characterMovementType, orientation = Vec3F.BACKWARD)
-}
 
 suspend fun SpaceEngineers.blockingRotateUntilOrientationForward(
     finalOrientation: Vec3F,
--- JvmClient/src/jvmMain/kotlin/environments/SeEnvironment.kt
+++ JvmClient/src/jvmMain/kotlin/environments/SeEnvironment.kt
@@ -6,7 +6,6 @@ import eu.iv4xr.framework.mainConcepts.WorldModel
 import eu.iv4xr.framework.spatial.meshes.Mesh
 import spaceEngineers.controller.ContextControllerWrapper
 import spaceEngineers.controller.SpaceEngineersTestContext
-import spaceEngineers.controller.extensions.moveForward
 import spaceEngineers.model.Block
 import spaceEngineers.model.CharacterMovementType
 import spaceEngineers.model.CharacterObservation
@@ -15,6 +14,7 @@ import spaceEngineers.model.Observation
 import spaceEngineers.model.ToolbarLocation
 import spaceEngineers.model.Vec3F
 import spaceEngineers.model.extensions.centerPosition
+import spaceEngineers.model.extensions.normalizeAsMovement
 import java.io.File
 import java.lang.Thread.sleep
 
@@ -128,7 +128,9 @@ class SeEnvironment(
     }
 
     fun moveForward(): WorldModel {
-        return controller.character.moveForward(CharacterMovementType.RUN).toWorldModel()
+        return controller.character.moveAndRotate(
+            movement = Vec3F.FORWARD.normalizeAsMovement(CharacterMovementType.RUN)
+        ).toWorldModel()
     }
 
     fun equipAndPlace(blockType: String) {
